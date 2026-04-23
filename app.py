c44ccf2c31ac6a65241457afe9055840d7f68d87
heltonhuamani-art <heltonhuamani@gmail.com>
2026-04-22 20:44:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 import streamlit as st
+
 import pandas as pd
+
 st.title("Manejo de Dataframes")
 st.sidebar.title("Herramientas")
 
 archivo = st.sidebar.file_uploader("Seleccione su archivo")
-datos = pd.read_csv(archivo)
 
-st.write(datos)
+if archivo is not None:
+
+ st.write("Su archivo ha sido cargado")
+
+    if archivo.name.endswith(".csv"):
+        datos = pd.read_csv(archivo)
+    elif archivo.name.endswith(".xlsx"):
+        datos = pd.read_excel(archivo)
+
+else:
+  
+
+    st.write("Cargue el archivo ")
